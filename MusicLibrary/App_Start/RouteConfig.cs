390f44635a8b9393e64436a03a45b5ff5caad246
christa89 <christa.bsc@gmail.com>
2017-08-18 11:45:02 +0530

    comit done

---
+++ MusicLibrary/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RouteConfig.cs" company="">
+//   Copyright © 2017 
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace App.MusicLibrary
+{
+    using System.Web.Routing;
+
+    using App.MusicLibrary.Routing;
+
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.Add("Default", new DefaultRoute());
+        }
+    }
+}
